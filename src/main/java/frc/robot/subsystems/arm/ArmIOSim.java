143d3bd551d74801eedd66cc2bf78268a6b5dc51
Kerem Alp <keremalpakkaya0508@gmail.com>
2024-08-02 19:23:02 +0300

    Add arm sim component

--- src/main/java/frc/robot/subsystems/arm/ArmIOSim.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIOSim.java
@@ -5,8 +5,14 @@
 package frc.robot.subsystems.arm;
 
 import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StructArrayPublisher;
+import edu.wpi.first.networktables.StructPublisher;
 import edu.wpi.first.wpilibj.simulation.SingleJointedArmSim;
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
 import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
@@ -44,12 +50,20 @@ public class ArmIOSim implements ArmIO {
 
   private double volts = 0.0;
 
+  private final Pose3d pivotPosition = new Pose3d(0.1,0.1,0.1, new Rotation3d(0, 0, 0));
+
+  //
+   StructPublisher<Pose3d> publisher = NetworkTableInstance.getDefault()
+    .getStructTopic("MyPose", Pose3d.struct).publish();
+   
   @Override
   public void updateInputs(ArmIOInputs inputs) {
     sim.update(0.02);
     inputs.armPosition = Math.toDegrees(sim.getAngleRads());
     m_arm.setAngle(Math.toDegrees(sim.getAngleRads()));
     SmartDashboard.putData("Arm Sim", m_mech2d);
+
+    publisher.set(pivotPosition);
   }
 
   @Override
